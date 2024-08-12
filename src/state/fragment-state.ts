34dccc8885b457488de2b75cd1ebf1aa222cce7b
inerttila <inert.etila@gmail.com>
2024-08-12 18:02:56 +0200

    just save

--- src/state/fragment-state.ts
+++ src/state/fragment-state.ts
@@ -1,11 +1,19 @@
 // Portions of this file are Copyright 2021 Google LLC, and licensed under GPL2+. See COPYING.
 
 import { State } from "./app-state";
-import { validateArray, validateBoolean, validateString, validateStringEnum } from "../utils";
+import {
+  validateArray,
+  validateBoolean,
+  validateString,
+  validateStringEnum,
+} from "../utils";
 import { defaultModelColor } from "./initial-state";
 
-export function buildUrlForStateParams(state: State) {//partialState: {params: State['params'], view: State['view']}) {
-  return `${location.protocol}//${location.host}${location.pathname}#${encodeStateParamsAsFragment(state)}`;
+export function buildUrlForStateParams(state: State) {
+  //partialState: {params: State['params'], view: State['view']}) {
+  return `${location.protocol}//${location.host}${
+    location.pathname
+  }#${encodeStateParamsAsFragment(state)}`;
 }
 export async function writeStateInFragment(state: State) {
   window.location.hash = await encodeStateParamsAsFragment(state);
@@ -15,25 +23,29 @@ async function compressString(input: string): Promise<string> {
     start(controller) {
       controller.enqueue(new TextEncoder().encode(input));
       controller.close();
-    }
+    },
   });
   // @ts-ignore
-  const compressedStream = stream.pipeThrough(new CompressionStream('gzip'));
+  const compressedStream = stream.pipeThrough(new CompressionStream("gzip"));
   const compressedData = await new Response(compressedStream).arrayBuffer();
   return btoa(String.fromCharCode(...new Uint8Array(compressedData)));
 }
 
 async function decompressString(compressedInput: string): Promise<string> {
-  const compressedData = Uint8Array.from(atob(compressedInput), c => c.charCodeAt(0));
+  const compressedData = Uint8Array.from(atob(compressedInput), (c) =>
+    c.charCodeAt(0)
+  );
   const stream = new ReadableStream<Uint8Array>({
     start(controller) {
       controller.enqueue(compressedData);
       controller.close();
-    }
+    },
   });
 
   // @ts-ignore
-  const decompressedStream = stream.pipeThrough(new DecompressionStream('gzip'));
+  const decompressedStream = stream.pipeThrough(
+    new DecompressionStream("gzip")
+  );
   const decompressedData = await new Response(decompressedStream).arrayBuffer();
   return new TextDecoder().decode(decompressedData);
 }
@@ -41,13 +53,13 @@ async function decompressString(compressedInput: string): Promise<string> {
 export function encodeStateParamsAsFragment(state: State) {
   const json = JSON.stringify({
     params: state.params,
-    view: state.view
+    view: state.view,
   });
   // return encodeURIComponent(json);
   return compressString(json);
 }
 export async function readStateFromFragment(): Promise<State | null> {
-  if (window.location.hash.startsWith('#') && window.location.hash.length > 1) {
+  if (window.location.hash.startsWith("#") && window.location.hash.length > 1) {
     try {
       const serialized = window.location.hash.substring(1);
       let obj;
@@ -57,7 +69,7 @@ export async function readStateFromFragment(): Promise<State | null> {
         // Backwards compatibility
         obj = JSON.parse(decodeURIComponent(serialized));
       }
-      const {params, view} = obj;
+      const { params, view } = obj;
       return {
         params: {
           sourcePath: validateString(params?.sourcePath),
@@ -67,18 +79,25 @@ export async function readStateFromFragment(): Promise<State | null> {
         },
         view: {
           layout: {
-            mode: validateStringEnum(view?.layout?.mode, ['multi', 'single']),
-            focus: validateStringEnum(view?.layout?.focus, ['editor', 'viewer', 'customizer'], s => false),
-            editor: validateBoolean(view?.layout['editor']),
-            viewer: validateBoolean(view?.layout['viewer']),
-            customizer: validateBoolean(view?.layout['customizer']),
+            mode: validateStringEnum(view?.layout?.mode, ["multi", "single"]),
+            focus: validateStringEnum(
+              view?.layout?.focus,
+              ["editor", "viewer", "customizer"],
+              (s) => false
+            ),
+            editor: validateBoolean(view?.layout["editor"]),
+            viewer: validateBoolean(view?.layout["viewer"]),
+            customizer: validateBoolean(view?.layout["customizer"]),
           },
-          collapsedCustomizerTabs: validateArray(view?.collapsedCustomizerTabs, validateString),
+          collapsedCustomizerTabs: validateArray(
+            view?.collapsedCustomizerTabs,
+            validateString
+          ),
           color: validateString(view?.color, () => defaultModelColor),
           showAxes: validateBoolean(view?.layout?.showAxis, () => true),
           showShadows: validateBoolean(view?.layout?.showShadow, () => true),
-          lineNumbers: validateBoolean(view?.layout?.lineNumbers, () => false)
-        }
+          lineNumbers: validateBoolean(view?.layout?.lineNumbers, () => false),
+        },
       };
     } catch (e) {
       console.error(e);
