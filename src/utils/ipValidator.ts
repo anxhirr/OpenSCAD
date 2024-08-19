1018f521c6e5eed9b8e70fec162593c3f49d6ca4
inerttila <inert.etila@gmail.com>
2024-08-19 16:39:43 +0200

    rm

--- src/utils/ipValidator.ts
+++
@@ -1,9 +0,0 @@
-import { Address6, Address4 } from "ip-address";
-
-export const isValidIPv4 = (ip: string): boolean => {
-  return Address4.isValid(ip);
-};
-
-export const isValidIPv6 = (ip: string): boolean => {
-  return Address6.isValid(ip);
-};
