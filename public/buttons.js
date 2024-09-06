76f40247fea8a3e527ec3c7b8efbf597ec2af08a
inerttila <inert.etila@gmail.com>
2024-09-06 17:32:42 +0200

    add the js with the cosutmize paths  and links for the images ect

---
+++ public/buttons.js
@@ -0,0 +1,101 @@
+// the "All" category
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("yourComponent", () => ({
+    cards: [
+      {
+        image: "https://cdn.mos.cms.futurecdn.net/Paxp7F5dLhbaMwT7K2LxoM.jpg",
+        title: "Airless Ping Pong Ball #1",
+        category: "Scad Model",
+        price: 0.2,
+        path: "https://example.com/edit/1",
+      },
+      {
+        image: "https://cdn.mos.cms.futurecdn.net/Paxp7F5dLhbaMwT7K2LxoM.jpg",
+        title: "Airless Ping Pong Ball #2",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/2",
+      },
+      {
+        image: "https://cdn.mos.cms.futurecdn.net/Paxp7F5dLhbaMwT7K2LxoM.jpg",
+        title: "Airless Ping Pong Ball #3",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/3",
+      },
+    ],
+  }));
+});
+
+// The "Art" category
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("yourComponent2", () => ({
+    cards: [
+      {
+        image: "https://cdn.mos.cms.futurecdn.net/Paxp7F5dLhbaMwT7K2LxoM.jpg",
+        title: "Airless Ping Pong Ball #1",
+        category: "Scad Model",
+        price: 0.2,
+        path: "https://example.com/edit/1",
+      },
+      {
+        image:
+          "https://www.todaynftnews.com/wp-content/uploads/2022/12/5-Dec-1-1.jpg",
+        title: "Airless Ping Pong Ball #2",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/2",
+      },
+      {
+        image:
+          "https://www.todaynftnews.com/wp-content/uploads/2022/12/5-Dec-1-1.jpg",
+        title: "Airless Ping Pong Ball #3",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/3",
+      },
+    ],
+  }));
+});
+
+// The "Music" category
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("yourComponent3", () => ({
+    cards: [
+      {
+        image: "https://cdn.mos.cms.futurecdn.net/Paxp7F5dLhbaMwT7K2LxoM.jpg",
+        title: "Airless Ping Pong Ball #1",
+        category: "Scad Model",
+        price: 0.2,
+        path: "https://example.com/edit/1",
+      },
+      {
+        image:
+          "https://www.todaynftnews.com/wp-content/uploads/2022/12/5-Dec-1-1.jpg",
+        title: "Airless Ping Pong Ball #2",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/2",
+      },
+      {
+        image:
+          "https://www.todaynftnews.com/wp-content/uploads/2022/12/5-Dec-1-1.jpg",
+        title: "Airless Ping Pong Ball #3",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/3",
+      },
+      {
+        image:
+          "https://www.todaynftnews.com/wp-content/uploads/2022/12/5-Dec-1-1.jpg",
+        title: "Airless Ping Pong Ball #3",
+        category: "Scad Model",
+        price: 0.3,
+        path: "https://example.com/edit/3",
+      },
+    ],
+  }));
+});
