f0ebc0860e1221b392b21811d2596b2c9e327cd6
inerttila <inert.etila@gmail.com>
2024-09-17 09:35:22 +0200

    edit

--- proxy_server.py
+++ proxy_server.py
@@ -3,18 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-# The address of the original Node.js app
 ORIGINAL_URL = 'http://172.27.21.59:4000'
-
-# The address where you want to forward the requests
-FORWARD_URL = 'http://192.168.40.14:8000'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-    # Forward the request to the original URL
-    response = requests.get(f'{ORIGINAL_URL}/{path}', params=request.args)
-    # Return the response from the original URL
+    target_url = f'{ORIGINAL_URL}/{path}'
+    response = requests.get(target_url, params=request.args)
     return Response(response.content, content_type=response.headers['Content-Type'])
 
 if __name__ == '__main__':
