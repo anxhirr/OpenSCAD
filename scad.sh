ea8e578e5bfe82df78bc699807b49ac981142b1e
inerttila <inert.etila@gmail.com>
2024-09-27 14:37:30 +0200

    edit

--- scad.sh
+++ scad.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-
-# Function to display usage
 usage() {
     echo "Usage: ./scad.sh [--dev | --prod]"
     exit 1
 }
 
-# Ensure start.sh is executable and run it
 if [ -f "./start.sh" ]; then
     chmod +x ./start.sh
     ./start.sh
 
-    # Check if start.sh was successful
     if [ $? -ne 0 ]; then
         echo "Error: start.sh failed!"
         exit 1
     fi
 
-    # Source .bashrc again to ensure Node.js, npm, and yarn are available
     source ~/.bashrc
 
 else
@@ -25,8 +20,8 @@ else
     exit 1
 fi
 
-# Check the argument passed to scad.sh
 if [ "$1" == "--dev" ]; then
+    echo " "
     echo "Running in development mode..."
     echo "--------------------------------------------------------"
     echo " "
@@ -34,13 +29,17 @@ if [ "$1" == "--dev" ]; then
     npm start
 
 elif [ "$1" == "--prod" ]; then
+    echo " "
     echo "Running in production mode..."
+    echo " "
     echo "--------------------------------------------------------"
     echo " "
     echo "Test and Build  the  production mode..."
+    echo " "
     npm run build:prod
 
     echo " Start the production mode "
+    echo " "
     npm run start:prod
     
 
--- start.sh
+++ start.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Install required packages
+echo "--------------------------------------------------------"
 sudo apt update
+
+echo "--------------------------------------------------------"
 sudo apt install -y wget make zip unzip
 
-# Install NVM
+echo "--------------------------------------------------------"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
-# Load NVM
+echo "--------------------------------------------------------"
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js using NVM
 nvm install node
+echo "--------------------------------------------------------"
+
+make public
+echo "--------------------------------------------------------"
+
+yarn install
