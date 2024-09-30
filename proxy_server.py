6493b5ddf9102944d1231ee492169a0e486b824d
inerttila <inert.etila@gmail.com>
2024-09-30 10:08:45 +0200

    edit port

--- proxy_server.py
+++ proxy_server.py
@@ -13,4 +13,4 @@ def proxy(path):
     return Response(response.content, content_type=response.headers['Content-Type'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=3000)
